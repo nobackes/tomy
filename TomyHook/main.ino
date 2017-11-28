318b2bc85a2918973b55bf2aaf90d85fbe519941
nobackes <ylan.rich@gmail.com>
2017-11-28 13:08:33 +0100

    Add files via upload

---
+++ TomyHook/main.ino
@@ -0,0 +1,59 @@
+
+#define X_DIR_PIN          55
+#define X_STEP_PIN         54
+#define X_ENABLE_PIN       38
+
+#define Y_DIR_PIN          61
+#define Y_STEP_PIN         60
+#define Y_ENABLE_PIN       56
+
+#define Z_DIR_PIN          48
+#define Z_STEP_PIN         46
+#define Z_ENABLE_PIN       62
+
+#define X_STEP_HIGH             PORTF |=  0b00000001;
+#define X_STEP_LOW              PORTF &= ~0b00000001;
+
+#define Y_STEP_HIGH             PORTF |=  0b01000000;
+#define Y_STEP_LOW              PORTF &= ~0b01000000;
+
+#define Z_STEP_HIGH             PORTL |=  0b00001000;
+#define Z_STEP_LOW              PORTL &= ~0b00001000;
+
+#define TIMER1_INTERRUPTS_ON    TIMSK1 |=  (1 << OCIE1A);
+#define TIMER1_INTERRUPTS_OFF   TIMSK1 &= ~(1 << OCIE1A);
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  stepperSetup();
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  for (int i = 0; i < NUM_STEPPERS; i++) {
+    prepareMovement( i, 800 );
+    runAndWait();
+  }
+
+  prepareMovement( 0, 8000 );
+  prepareMovement( 1,  800 );
+  prepareMovement( 2, 2400 );
+  
+  runAndWait();
+
+  delay(1000);
+
+  prepareMovement( 0, -8000 );
+  prepareMovement( 1,  1600 );
+  prepareMovement( 2, -2400 );
+  
+  runAndWait();
+
+  while (true);
+  
+}
